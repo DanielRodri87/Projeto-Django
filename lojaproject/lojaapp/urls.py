d367df2c13e84e8c23a20efc40f6b189135be48e
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-01-27 18:32:23 -0300

    Funcionando

--- lojaproject/lojaapp/urls.py
+++ lojaproject/lojaapp/urls.py
@@ -5,5 +5,7 @@ app_name = "lojaapp"
 
 urlpatterns = [
     path("",HomeView.as_view(), name="home"),
+    path("sobre/",SobreView.as_view(), name="sobre"),
+    path("contato/",ContatoView.as_view(), name="contato"),
     
 ]
--- lojaproject/lojaapp/views.py
+++ lojaproject/lojaapp/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "home.html"
+    
+class SobreView(TemplateView):
+    template_name = "sobre.html"
+    
+class ContatoView(TemplateView):
+    template_name = "contato.html"
